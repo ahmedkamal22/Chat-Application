b152760e35067488d1db99d44eb2eb20836b6935
ahmedkamal22 <ak0850000@gmail.com>
2021-06-07 23:10:41 +0300

    Base Activity is created Made Login Activity working by it

--- app/src/main/java/com/kamal/chatapplication/login/LoginActivity.kt
+++ app/src/main/java/com/kamal/chatapplication/login/LoginActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.islami.base.BaseActivity
 import com.kamal.chatapplication.R
 import com.kamal.chatapplication.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var viewModel: LoginViewModel
-    lateinit var binding: ActivityLoginBinding
+class LoginActivity : BaseActivity<ActivityLoginBinding,LoginViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        binding.vm = viewModel
+        viewDataBinding.vm = viewModel
+    }
+
+    override fun intializeViewModel(): LoginViewModel {
+        return ViewModelProvider(this).get(LoginViewModel::class.java)
+    }
+
+    override fun getLayoutID(): Int {
+        return R.layout.activity_login
     }
 }
